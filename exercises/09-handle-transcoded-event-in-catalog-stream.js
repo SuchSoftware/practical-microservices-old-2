7576e0f596879fe1e3c216cd14b8c10a651b5f84
Ethan Garofolo <ethan@suchsoftware.com>
2020-02-14 10:13:40 -0700

    Write events on exercise 9

--- exercises/09-handle-transcoded-event-in-catalog-stream.js
+++ exercises/09-handle-transcoded-event-in-catalog-stream.js
@@ -1,17 +1,31 @@
 const uuid = require('uuid/v4')
 
 const { config } = require('./preamble')
+const StartedControls = require('../src/catalog-component/controls/events/started')
 const TranscodedControls = require('../src/catalog-component/controls/events/transcoded')
 
+const traceId = uuid()
+const videoId = uuid()
+const streamName = `catalog-${videoId}`
+
+const started = StartedControls.example()
+started.metadata.traceId = traceId
+started.streamName = streamName
+
 const transcoded = TranscodedControls.example()
-transcoded.metadata.traceId = uuid()
-transcoded.streamName = `catalog-${uuid()}`
+transcoded.metadata.traceId = traceId
+transcoded.streamName = streamName
 
 // Run this immediately.  It will go boom.  In this exercise, you're going to
 // fill out the whole handler, including defining it in the handlers object.
 // That happens in `src/catalog-component/index.js`.
-config.catalogComponent.eventHandlers
-  .Transcoded(transcoded)
+//
+// For this exercise we need precursor events to actually be in the
+// Message Store
+config.messageStore
+  .write(streamName, started)
+  .then(() => config.messageStore.write(streamName, transcoded))
+  .then(() => config.catalogComponent.eventHandlers.Transcoded(transcoded))
   .then(() => config.catalogComponent.eventHandlers.Transcoded(transcoded))
   .then(() => console.log('Transcoded processed.'))
   .finally(config.messageStore.stop)
